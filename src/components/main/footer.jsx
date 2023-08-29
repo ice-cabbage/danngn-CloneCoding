731577935d62b3891af3b3c4f646a611eee1f500
Cabbage-shaved-ice <142379337+ice-cabbage@users.noreply.github.com>
2023-08-29 17:20:45 +0900

    footer update
    footer information

--- src/components/main/footer.jsx
+++ src/components/main/footer.jsx
@@ -4,12 +4,15 @@ import styled from "styled-components"
 const FooterWrapper = styled.div`
     width: 100%;
     height: 550.66px;
+    background-color: #ffffff;
 `
 
 function Footer() {
     return (
         <FooterWrapper>
-            
+            <CompanyInformation>
+                
+            </CompanyInformation>
         </FooterWrapper>
     );
 }
